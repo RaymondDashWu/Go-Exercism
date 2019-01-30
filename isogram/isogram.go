feb69660bf1d48d6f1b20b128431839d13c8ad13
RaymondDashWu <raymond31670@hotmail.com>
2019-01-29 16:51:02 -0800

    [BLOCKED] Trying hash table isogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,7 +1,5 @@
 package isogram
 
-import "unicode"
-
 // PSEUDO BRAINSTORM:
 // iterate through letters in word
 // append to array
@@ -10,27 +8,31 @@ import "unicode"
 // emptySlice = {B: 1, A: 1, T: 1}
 // BAT
 func IsIsogram(a string) bool {
-	var emptySlice = []string{}
-	// m := make(map[string]int)
-	for i := 0; i < len(a); i++ {
-		// rune will return int?
-		if unicode.IsLetter(rune(a[i])) {
-			emptySlice = append(emptySlice, string(a[i]))
-			// check if letter in emptySlice
+	// hash table method
+	empty_dict := make(map[string]intg)
 
-			for ind, v := range emptySlice {
-				// emptySlice = append(emptySlice, string(a[i]))
-				if v == emptySlice[ind] {
-					return false
-				}
-			}
-			// if letter in emptySlice:
-			// return False
-		} else {
-			continue
-		}
-	}
-	return true
+	// var emptySlice = []string{}
+	// // m := make(map[string]int)
+	// for i := 0; i < len(a); i++ {
+	// 	// rune will return int?
+	// 	if unicode.IsLetter(rune(a[i])) {
+	// 		emptySlice = append(emptySlice, string(a[i]))
+	// 		fmt.Println("Appended:", emptySlice)
+	// 		// check if letter in emptySlice
+
+	// 		for ind, v := range emptySlice {
+	// 			// emptySlice = append(emptySlice, string(a[i]))
+	// 			if v == emptySlice[ind] {
+	// 				return false
+	// 			}
+	// 		}
+	// 		// if letter in emptySlice:
+	// 		// return False
+	// 	} else {
+	// 		continue
+	// 	}
+	// }
+	// return true
 }
 
 // for _, b := range list {
