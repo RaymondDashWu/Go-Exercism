5ef7e987428bf0a37c90a0e4cbbc1d4e33a9a05e
RaymondDashWu <raymond31670@hotmail.com>
2019-01-30 23:07:08 -0800

    [BLOCKED] Type conversion luhn string to int

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -3,13 +3,22 @@ package luhn
 // PSEUDO Brainstorm:
 // double every other number starting with index 0
 // pass every other number starting with index 1
-func Valid(num int) int {
-	for i := 0; i < len(num); i += 2 {
-		total := 0
-		total = num[i] * 2
-		if total > 9 {
-			total = num[i] - 9
+func Valid(num string) bool {
+	var total = 0
+	for i := 0; i < len(num); i++ {
+		// var total = 0
+		if i%2 == 0 {
+			int(num[i]) *= 2
+			if num[i] > 9 {
+				num[i] -= 9
+				total += num[i]
+			}
+		} else {
+			total += num[i]
 		}
-		return total
 	}
+	if total%10 == 0 {
+		return true
+	}
+	return false
 }
