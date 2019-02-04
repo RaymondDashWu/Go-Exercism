30a378dc0b8f4fe2c2af290535f2eeff6ad2cf0e
RaymondDashWu <raymond31670@hotmail.com>
2019-02-03 18:19:43 -0800

    [ADD] SumMultiples close. Forgot to account for unique numbers

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,14 +1,19 @@
 package luhn
 
+import "strconv"
+
 // PSEUDO Brainstorm:
 // double every other number starting with index 0
 // pass every other number starting with index 1
+// return all of these numbers added together
+// if total % 10 = 0, return true; else false
 func Valid(num string) bool {
 	var total = 0
 	for i := 0; i < len(num); i++ {
 		// var total = 0
 		if i%2 == 0 {
-			int(num[i]) *= 2
+			// Int(num[i]) *= 2
+			strconv.Atoi(string(num[i])) *= 2
 			if num[i] > 9 {
 				num[i] -= 9
 				total += num[i]
---
+++ sum-of-multiples/sum_of_multiples.go
@@ -0,0 +1,28 @@
+package summultiples
+
+import "fmt"
+
+// PSEUDO BRAINSTORM:
+// Given a number find multiples up to that number that are divisible by given numbers then add them together
+// Ex: Given 20, find UNIQUE numbers up to but not including 20 that are divisible by 3 & 5
+// Solution: 3, 5, 6, 9, 10, 12, 15, and 18. Added together equals 78
+
+// start out with a total variable set to 0. Will get added to as modulo operator finds numbers
+// for all numbers in the max number - 1 divide by array of ints in divisors
+// how to best go through array of divisors? Another for loop adds would make it n^2 operation
+
+func SumMultiples(limit int, divisors []int) int {
+	var sum = 0
+	for i := 0; i < limit; i++ {
+		for _, v := range divisors {
+			if i%v == 0 {
+				sum += i
+				fmt.Println("number:", i)
+				fmt.Println("divisible by:", v)
+				fmt.Println("sum:", sum)
+				fmt.Println("")
+			}
+		}
+	}
+	return sum
+}
