b7d484aceb4f6e7fb0081aa738c493b2326d3c8f
RaymondDashWu <raymond31670@hotmail.com>
2019-02-06 22:57:59 -0800

    Night cap

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,6 +1,10 @@
 package luhn
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
 
 // PSEUDO Brainstorm:
 // double every other number starting with index 0
@@ -8,22 +12,30 @@ import "strconv"
 // return all of these numbers added together
 // if total % 10 = 0, return true; else false
 func Valid(num string) bool {
+	fmt.Println("Tested against num:", num)
 	var total = 0
 	for i := 0; i < len(num); i++ {
-		// var total = 0
-		if i%2 == 0 {
-			// Int(num[i]) *= 2
-			// STUCK converting below line to an int. num string to int
-			strconv.Atoi(string(num[i])) *= 2
-			if num[i] > 9 {
-				num[i] -= 9
-				total += num[i]
+		if unicode.IsNumber(rune(num[i])) {
+			digit, _ := strconv.Atoi(string(num[i]))
+			fmt.Println("digit:", digit)
+			// Below line does not work because starts at index 0
+			if i%2 == 0 {
+				// Int(num[i]) *= 2
+				digit, _ := strconv.Atoi(string(num[i]))
+				digit *= 2
+				total += digit * 2
+				fmt.Println("total in first if:", total)
+				if digit > 9 {
+					digit -= 9
+					total += digit
+				}
+			} else {
+				total += digit
+				fmt.Println("total in else:", total)
 			}
-		} else {
-			total += num[i]
 		}
 	}
-	if total%10 == 0 {
+	if total%10 == 0 && len(num) > 1 {
 		return true
 	}
 	return false
