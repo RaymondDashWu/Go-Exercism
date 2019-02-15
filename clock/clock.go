c046c57a8d1df4cb7bf248dfb67fd7880a627d2e
RaymondDashWu <raymond31670@hotmail.com>
2019-02-15 15:50:21 -0800

    Initial Commit

---
+++ clock/clock.go
@@ -0,0 +1,29 @@
+
+// Hint
+// time module parsetime duration
+package clock
+
+
+// PSEUDO Brainstorm
+// 24 hour clock would be easier
+// hours, minutes, subtractions/additions
+// Needs to work with ==
+
+// sets the base time for the clock. Returns time in 24 hour format
+func New {
+
+}
+
+
+func String {
+
+}
+
+// accepts 1 arguments. Addition
+func Add {
+
+}
+
+func Subtract {
+
+}
