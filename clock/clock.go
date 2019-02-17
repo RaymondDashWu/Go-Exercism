f242ec78fc1b38ad3b8d0b585822faa32758e82d
RaymondDashWu <raymond31670@hotmail.com>
2019-02-16 22:09:27 -0800

    [ADD] Clock v1 solution

--- clock/clock.go
+++ clock/clock.go
@@ -2,13 +2,16 @@
 // time module parsetime duration
 package clock
 
-import "time"
+import "fmt"
 
 // PSEUDO Brainstorm
 // 24 hour clock would be easier
 // hours, minutes, subtractions/additions
 // Needs to work with ==
 // sets the base time for the clock. Returns time in 24 hour format
+
+// !!!!! IDEA: Make a clock that revolves around minutes rather than using any functions within Go
+// https://stackoverflow.com/questions/30956224/go-how-to-format-number-digit-count
 
 // Resource: https://golangbot.com/inheritance/
 
@@ -17,28 +20,30 @@ import "time"
 // (Clock) Add(minutes int) Clock
 // (Clock) Subtract(minutes int) Clock
 
-type Clock struct {
-	h int
-	m int
-}
+type Clock int
+
+const (
+	hour = 60
+	day  = 24 * hour
+)
 
 // Creates a new 24 hour clock period
-func New(hours int, minutes int) Clock {
-	t, _ = time.Parse(time.RFC822, "2013-Feb-03")
+func New(h int, m int) Clock {
+	return Clock(0).Add(hour*h + m)
 }
 
 // Takes two arguments, layout string and string to format. The layout string must use the exact times specified in the docs as a reference. Ex: parsestring(“12:15”, “00:00”)
 // Returns a time object u can call .Hours() to get hours and .Minutes() to get respective hours and minutes on of the time object
 func (c Clock) String() string {
-
+	return fmt.Sprintf("%02d:%02d", c/hour, c%hour)
 }
 
 // accepts 1 arguments. Minutes to add
-func (c Clock) Add(minAdd int) Clock {
-	c.h
+func (c Clock) Add(min int) Clock {
+	return Clock((int(c) + day + min%day) % day)
 }
 
 // accepts 1 arguments. Minutes to subtract
-func (c Clock) Subtract(minSub int) Clock {
-
+func (c Clock) Subtract(min int) Clock {
+	return Clock((int(c) + day - min%day) % day)
 }
