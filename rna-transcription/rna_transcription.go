dcfc9b6d79250916cf85d7944ce2fee89fee8889
RaymondDashWu <raymond31670@hotmail.com>
2019-02-19 20:23:22 -0800

    Initial Commit

---
+++ rna-transcription/rna_transcription.go
@@ -0,0 +1,17 @@
+package strand
+
+// Notes
+// G -> C
+// C -> G
+// T -> A
+// A -> U
+
+func ToRNA(dna string) string {
+	if len(dna) >= 1 {
+		for i := 0; i < len(dna); i++ {
+			if dna[i] := "G" {
+				dna[i] == "C"
+			}
+		}
+	}
+}
