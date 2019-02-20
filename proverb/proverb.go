cf150dfb078f71370eb754dd63906985e7e2431d
RaymondDashWu <raymond31670@hotmail.com>
2019-02-19 18:29:43 -0800

    [ADD] Proverb completed

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -13,21 +13,22 @@
 // And all for the want of a *.
 package proverb
 
+import "fmt"
+
+// ["nail", "shoe", "horse", "rider", "message", "battle", "kingdom"]
+
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
 	// Write some code here to pass the test suite.
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-
-	empty_string := []rune(input)
-	// fmt.Println("empty_string:", empty_string)
-
-	for i, j := 0, len(rhyme)-1; i > j; i, j = i+1, j+2 {
-		empty_string[i], empty_string[j] = empty_string[j], empty_string[i]
-		// fmt.Println(empty_string)
+	var total_rhyme = []string{}
+	if len(rhyme) >= 1 {
+		for i := 0; i < len(rhyme)-1; i++ {
+			total_rhyme = append(total_rhyme, fmt.Sprintf("For want of a %v the %v was lost.", rhyme[i], rhyme[i+1]))
+		}
+		total_rhyme = append(total_rhyme, fmt.Sprintf("And all for the want of a %v.", rhyme[0]))
 	}
-	return string(empty_string)
-
-	return []string{}
+	return total_rhyme
 }
