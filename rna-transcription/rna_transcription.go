f7974a9610d3611258959540096b5429bf96c7d1
RaymondDashWu <raymond31670@hotmail.com>
2019-02-19 22:04:09 -0800

    [ADD] RNA Transcription completed

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -7,11 +7,22 @@ package strand
 // A -> U
 
 func ToRNA(dna string) string {
+	var emptyString = ""
 	if len(dna) >= 1 {
 		for i := 0; i < len(dna); i++ {
-			if dna[i] := "G" {
-				dna[i] == "C"
+			if dna[i] == 'G' {
+				emptyString += string('C')
+			}
+			if dna[i] == 'C' {
+				emptyString += string('G')
+			}
+			if dna[i] == 'T' {
+				emptyString += string('A')
+			}
+			if dna[i] == 'A' {
+				emptyString += string('U')
 			}
 		}
 	}
+	return emptyString
 }
