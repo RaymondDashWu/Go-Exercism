e45575d789852045c2a154b38bd800c5406292c4
RaymondDashWu <raymond31670@hotmail.com>
2019-02-20 18:10:10 -0800

    [ADD] Nucleotide assignment finished

--- nucleotide-count/nucleotide_count.go
+++ nucleotide-count/nucleotide_count.go
@@ -1,8 +1,10 @@
 package dna
 
+import "errors"
+
 // Histogram is a mapping from nucleotide to its count in given DNA.
 // Choose a suitable data type.
-type Histogram map[string]int
+type Histogram map[rune]int
 
 // DNA is a list of nucleotides. Choose a suitable data type.
 type DNA string
@@ -13,20 +15,15 @@ type DNA string
 // Counts is a method on the DNA type. A method is a function with a special receiver argument.
 // The receiver appears in its own argument list between the func keyword and the method name.
 // Here, the Counts method has a receiver of type DNA named d.
+
 func (d DNA) Counts() (Histogram, error) {
-	var h = make(Histogram)
+	empty_d := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
 	for _, v := range d {
-		// Possibly syntax issue? Logic for histogram is correct
-		if v == h[string(v)] {
-			h[string(v)]++
+		if _, ok := empty_d[v]; ok {
+			empty_d[v]++
+		} else {
+			return empty_d, errors.New("Not a valid strand")
 		}
-		h[string(v)] = 1
 	}
-	// for i := 0; i < len(d); i++ {
-	// 	if d[i] in h {
-
-	// 	}
-	// }
-
-	return h, nil
+	return empty_d, nil
 }
