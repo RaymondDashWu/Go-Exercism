c9e006eaaa4501f6a5d481e0b7df2acbff39a35a
RaymondDashWu <raymond31670@hotmail.com>
2019-02-21 13:14:16 -0800

    [ADD] Initial commit + pseudocode

---
+++ roman-numerals/roman_numerals.go
@@ -0,0 +1,23 @@
+package romannumerals
+
+// Pseudo brainstorm
+// Symbol	I	V	X	L	C	D	M
+// Value	1	5	10	50	100	500	1,000
+// Each symbol can only be used 3 times
+// 0 can not be done
+
+// Pseudo Brainstorm:
+// Ordered dictionary that is iterated through?
+// Check to see if input number is bigger than biggest (M aka 1000)
+// if it is then subtract that amount up to 3 times. Otherwise move on to next roman numeral
+// add all results to empty roman numeral holder
+
+func ToRomanNumeral(num int) (string, bool) {
+	
+	var {}
+	rom_numeral_holder = ""
+	if num == 0 {
+		return "", false
+	}
+
+}
